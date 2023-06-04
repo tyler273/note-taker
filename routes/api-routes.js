f2622733f9416df9c5b723d15c88e764a2ad5b77
Tyler <t.woods95@aol.com>
2023-06-04 17:19:25 -0400

    Started post request

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,10 +2,23 @@ const router = require("express").Router();
 const fs = require("fs");
 const path = require("path");
 const db = require("../db/db.json");
+const uuid = require("../helpers/uuid");
 
 router.get("/notes", (req, res) => res.json(db));
 router.post("/notes", (req, res) => {
 
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save if title and text present
+    const newNote = {
+        title,
+        text,
+        id: uuid(),
+    }
+  }
 });
 
 module.exports = router;
